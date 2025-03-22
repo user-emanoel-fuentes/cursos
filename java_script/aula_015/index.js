999e9422a014464024a32a4c0722da0a5085a055
user-emanoel-fuentes <worknoel.cardoso@gmail.com>
2025-03-22 14:31:07 -0700

    feat: atualização curso js.

--- java_script/aula_015/index.js
+++ java_script/aula_015/index.js
@@ -1,3 +1,26 @@
-let num1 = 1;
-let num2 = 2.5;
+// Dificuldade da imprecisão por conta do IEEE 754 2008
+
+let num1 = 0.7;
+let num2 = 0.1;
+num1 += num2 // 0.8
+num1 += num2 // 0.9
+num1 += num2 // 1.0
+// console.log(num1); // 0.9999999999999999
+
+// 1º Forma: Multiplicando os valores do calculo por 100 depois dividindo por esse mesmo valor: 
+// num1 = ((num1 * 100) + (num2 * 100)) / 100
+// num1 = ((num1 * 100) + (num2 * 100)) / 100
+// num1 = ((num1 * 100) + (num2 * 100)) / 100
+// console.log(num1)
+
+// 2 º Forma: Trabalhando com as funções do JS no resultado final:
+// console.log(Number(num1.toFixed(2)))
+
+// Esses dois modelos farão a precisão ser exata.
+
+// toString() -> Altera um dado para string;
+// toFixed() -> Define quantidade de casa decimais;
+// Number.isInteger(numero) -> Valida se o número dentro do parenteses é inteiro;
+// Number.inNaN(valor) -> Retorna verdadeiro se o valor informado for NaN 
+// (Geralmente resultado de alguma ação numérica);
 
---
+++ java_script/aula_016/index.js
@@ -0,0 +1,25 @@
+// Pesquisar sobre o Math
+
+let num1 = 9
+// let num2 = Math.floor(num1) // arredonda para baixo
+// let num2 = Math.ceil(num1) // arredonda para cima 
+// let num2 = Math.round(num1); // arredonda para o mais próximo
+// console.log(Math.max(1,2,3,4,5,6,100,1500,-10,20)); // Retornar o maior número de uma lista de números
+// console.log(Math.min(1,2,3,4,5,6,100,1500,-10,20)); // Retornar o menor número de uma lista de números
+
+// console.log(Math.random()) // Gera números aleatórios entre 0 e 1
+// Gerar números aleatórios entrer um intervalo:
+    // let aleatorio = Math.random() * (25 - 5) + 5;
+    // console.log(aleatorio);
+
+    // Arredondar os valores aleatórios:
+    // let aleatorio = Math.round(Math.random() * (20 - 1) + 1);
+    // console.log(aleatorio);
+
+// OBSERVAÇÕES: 
+// Obtendo o valor da raiz quadrada:
+// console.log(num1**(1/2)) // ou 
+// console.log(num1**0.5)
+
+// O resultado de uma divisão por 0 em JS é: Infinity, isso é considerado True em uma validação boolean;
+// let num3 = console.log(100/0);
