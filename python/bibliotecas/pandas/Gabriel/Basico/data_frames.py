f731936042ee5a4df8aeb47a4e5fd3f88493bc68
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-04-02 18:13:47 -0300

    refactor: Renomear e mover múltiplos arquivos para melhor organização

--- python/bibliotecas/pandas/Gabriel/Basico/data_frames.py
+++ python/bibliotecas/pandas/Gabriel/Basico/data_frames.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 # Criando um Dict para o nosso DataFrame:
 data = {
-  "name": [420, 380, 390],
+  "name": ["Gabriel", "Evandro", "Isac"],
   "age": [50, 40, 45],
   "sex": ['M', 'F', 'M']
 }
 
 new_data = pd.DataFrame(data)
+
+print(new_data)
