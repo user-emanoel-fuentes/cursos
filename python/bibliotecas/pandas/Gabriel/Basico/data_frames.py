0a944e1e4ed8c8dbd747663f267bc4fb0f6423ac
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-04-02 19:26:25 -0300

    feat: mais sobre DataFrames e leitura de arquivos .csv

--- python/bibliotecas/pandas/Gabriel/Basico/data_frames.py
+++ python/bibliotecas/pandas/Gabriel/Basico/data_frames.py
@@ -3,12 +3,32 @@
 import pandas as pd
 
 # Criando um Dict para o nosso DataFrame:
+# data = {
+#   "name": ["Gabriel", "Maria", "Isac"],
+#   "age": [50, 40, 45],
+#   "sex": ['M', 'F', 'M']
+# }
+
+# new_data = pd.DataFrame(data)
+
+# Imprimindo nosso DataFrame:
+# print(new_data)
+
+# Conseguimos selecionar uma das linhas do nosso DataFrame usando ".loc[]" e dentro dos colchetes
+# selecionarmos a linha que queremos em formato de indexes (começando do 0):
+# print(new_data.loc[0])
+
+# Podemos imprimir uma lista de linhas que queremos:
+# # print(new_data.loc[[0, 1]])
+
 data = {
-  "name": ["Gabriel", "Evandro", "Isac"],
+  "name": ["Gabriel", "Maria", "Isac"],
   "age": [50, 40, 45],
   "sex": ['M', 'F', 'M']
 }
 
-new_data = pd.DataFrame(data)
+# Nomeando index do DataFrame:
+new_data = pd.DataFrame(data, index= ["user1", "user2", "user3"])
 
-print(new_data)
+# Imprimindo linhas com base nos nomes (indexes):
+print(new_data.loc[["user1", "user2"]])
