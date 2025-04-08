89c3e9797fbe63801d38cda7e2713b1669c1eca9
user-emanoel-fuentes <worknoel.cardoso@gmail.com>
2025-04-08 15:14:30 -0700

    feat: atualização curso js.

---
+++ java_script/ldp/aula_035/index.js
@@ -0,0 +1,39 @@
+// JS: Laços de repetição (For in):
+
+const frutas = ["Melância", "Pera", "Abacate", "Maçã", "Uva"]
+
+// Tradicional: Passea por meio de um contador 
+for (let i = 0; i < frutas.length; i++) {
+    // console.log(frutas[i]);
+}
+
+// For in: Passea lendo os indices ou chaves do array/objeto;
+for (let i in frutas) {
+//     console.log(i, frutas[i]);
+}
+
+const pessoa = {
+    nome:"Nicolas",
+    sobrenome:"Alves",
+    idade:28,
+    sexo:"M",
+    endereço:{
+        rua:"Av Lorenço",
+        numero:789,
+    },
+}
+
+for (let key in pessoa) {
+    // console.log(key, pessoa[key]);
+}
+
+// for of: Não funciona para objetos, somente arrays/vetores
+const nome = "Luiz Otávio"
+for (let valor of nome) {
+    console.log(valor);
+}
+
+// forEach()
+frutas.forEach(function(valor, indice, array){
+    console.log(valor, indice, array);
+});
