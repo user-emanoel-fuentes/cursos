ad278e7bfd008fddfb3d6a9cf8a35ee6a83aacd3
user-emanoel-fuentes <worknoel.cardoso@gmail.com>
2025-04-09 08:36:11 -0700

    feat: atualização curso js.

--- java_script/ldp/aula_039/index.js
+++ java_script/ldp/aula_039/index.js
@@ -4,19 +4,19 @@
 
 // 1 - Escreva uma função que recebe dois números e retorna o maior deles. 
     // Resposta:
-    // function maior(x,y){ return x < y ? y : x }
+    function maior(x,y){ return x < y ? y : x }
 
     // Melhoria (Precisa de uma variável)
-    // const maior2 = (x,y) => x < y ? y : x 
+    const maior2 = (x,y) => x < y ? y : x 
     
     // console.log(maior2(5,7));
 
 // 2 - Escreva uma função chamada ePaisagem que recebe dois argumentos,
 // largura e altura de uma imagem. Retorne "true" se a imagem estiver no modo paisagem.
 
-    // function ePaisagem(largura, altura){ return largura > altura }
+    function ePaisagem(largura, altura){ return largura > altura }
 
-    // const ePaisagem2 = (largura, altura) => largura > altura;
+    const ePaisagem2 = (largura, altura) => largura > altura;
 
     // console.log(ePaisagem2(1080, 1920));
     // console.log(ePaisagem2(1920, 1080));
@@ -30,19 +30,19 @@
 // Não divisível por 3 e 5 retorna número
 
 // Minha resposta:
-// function fizzBuzz(n) {
-//     if (typeof(n) === "number") {
-//         if (0 === n % 5) {
-//             if (0 === n % 3) { return "FizzBuzz" }
-//             return "Buzz"
-//         } else if (0 === n % 3) { return "Fizz" }
-//         return n   
-//     }
-//     return n    
-// }
+function fizzBuzz(n) {
+    if (typeof(n) === "number") {
+        if (0 === n % 5) {
+            if (0 === n % 3) { return "FizzBuzz" }
+            return "Buzz"
+        } else if (0 === n % 3) { return "Fizz" }
+        return n   
+    }
+    return n    
+}
 
-
-function fizzBuzz(n){
+// Resposta Aula:
+function fizzBuzz2(n){
     if (typeof n !== "number") return n;
     if (n % 3 == 0 & n % 5 == 0) return "FizzBuzz";
     if (n % 3 == 0) return "Fizz";
@@ -52,4 +52,5 @@ function fizzBuzz(n){
 
 for (let i=0; i <=100; i++) {
     console.log(i, fizzBuzz(i));
+    console.log(i, fizzBuzz2(i));
 }
