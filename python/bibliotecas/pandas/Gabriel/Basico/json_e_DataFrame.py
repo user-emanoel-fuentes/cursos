59b4c9309489272f76a441b3452f322b97035162
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-04-10 00:46:08 -0300

    feat: Adicionando estudos sobre json e DataFrames.

--- python/bibliotecas/pandas/Gabriel/Basico/json_e_DataFrame.py
+++ python/bibliotecas/pandas/Gabriel/Basico/json_e_DataFrame.py
@@ -8,6 +8,49 @@ Os dados do json são compostos em formato de objeto.
 import pandas as pd
 
 # Como no exemplo do csv, aqui podemos 
-df = pd.read_json("data/data.json")
+# df = pd.read_json("data/data.json")
 
-print(df)
+# print(df)
+
+# Também podemos imprimir em formato de string:
+# print(df.to_string())
+
+# Em formato de dicionário (parecido com o json):
+data = {
+  "Duration":{
+    "0":60,
+    "1":60,
+    "2":60,
+    "3":45,
+    "4":45,
+    "5":60
+  },
+  "Pulse":{
+    "0":110,
+    "1":117,
+    "2":103,
+    "3":109,
+    "4":117,
+    "5":102
+  },
+  "Maxpulse":{
+    "0":130,
+    "1":145,
+    "2":135,
+    "3":175,
+    "4":148,
+    "5":127
+  },
+  "Calories":{
+    "0":409,
+    "1":479,
+    "2":340,
+    "3":282,
+    "4":406,
+    "5":300
+  }
+}
+
+df = pd.DataFrame(data)
+
+print(df) 
