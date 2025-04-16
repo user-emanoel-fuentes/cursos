72bbed686190284ac82754ea31e757010e0cbfcb
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-04-15 23:12:02 -0300

    docs: texto de introdução para formatação errada em dados.

---
+++ python/bibliotecas/pandas/Gabriel/Limpeza_de_dados/formato_errado.py
@@ -0,0 +1,8 @@
+'''
+Dados com formatação errada.
+
+Células com dados em formatos errados podem ser muito dificeis de se lidar e analisar.
+
+Para consertar este problema temos 2 opções: apagar as linhas com esses dados ou converter as células das colunas
+no mesmo formato.
+'''
