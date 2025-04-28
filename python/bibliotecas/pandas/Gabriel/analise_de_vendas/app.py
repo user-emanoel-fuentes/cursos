41f8a184391b7e6746bdbf283140a6b617b17914
Gabriel Maxsy <gabrielmaxsy@gmail.com>
2025-04-28 18:21:05 -0300

    docs: iniciando desafio de vendas.

---
+++ python/bibliotecas/pandas/Gabriel/analise_de_vendas/app.py
@@ -0,0 +1,17 @@
+"""
+Este script realiza o tratamento de dados utilizando o método "pandas" em Python para manipulação de planilhas Excel.
+
+Objetivo:
+- Ler os dados de uma planilha Excel (.xlsx) utilizando a biblioteca pandas.
+- Identificar se algum(a) vendedor(a) atingiu ou superou a marca de 55.000 vendas em algum mês.
+
+Funcionalidades:
+1. Leitura de dados de uma planilha Excel.
+2. Processamento e análise dos dados para verificar o desempenho dos vendedores.
+3. Exibição dos resultados indicando os vendedores que atingiram ou superaram a meta estabelecida de 55.000.
+
+Requisitos:
+- Biblioteca pandas instalada.
+- Arquivo Excel (.xlsx) contendo os dados de vendas.
+
+"""
